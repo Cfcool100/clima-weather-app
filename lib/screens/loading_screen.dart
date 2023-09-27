01e101f6bb4c46f6ac38a6ffddb5760e290663a9
Cfcool100 <cfcool100@gmail.com>
2023-09-27 00:42:43 +0000

    spinner + redirection + callApi

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,14 +10,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocateData();
+  }
+
+  void getLocateData() async {
+    var weatherData = WeatherModel().getLocationData();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(locationWeather: weatherData),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+      body: SafeArea(
+        child: Center(
+          child: SpinKitPianoWave(
+            size: 20,
+            color: Colors.white,
+          ),
         ),
       ),
     );
